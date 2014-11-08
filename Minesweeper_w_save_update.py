767db069a6160b8e8cecdafdb06c3f4467f06f89
Omar Hayes <ohh2102@users.noreply.github.com>
2014-11-08 12:58:40 -0500

    Update Minesweeper_w_save_update.py

--- Minesweeper_w_save_update.py
+++ Minesweeper_w_save_update.py
@@ -220,15 +220,15 @@ def Get_or_Play():
         for j in Board.board:
             if Board.board[j].save_val==-1:
                 Board.board[j].loc_mine.on_status=True
-                Board.board[j].loc_mine.avail=0
+                Board.board[j].avail=0
             elif Board.board[j].save_val==-2:
                 Board.board[j].loc_value='x'
-                Board.board[j].loc_mine.avail=0
+                Board.board[j].avail=0
             elif Board.board[j].save_val==0:
                 Board.board[j].loc_value==0
             else:
                 Board.board[j].loc_value=Board.board[j].save_val
-                Board.board[j].loc_mine.avail=0
+                Board.board[j].avail=0
         playing_status=True
         while playing_status==True:
             Board.print_board()
